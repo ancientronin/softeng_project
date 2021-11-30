c1f1fae9786bbaac6a9e0b4b2ae652476985ef39
ancientronin <roblewis8855@gmail.com>
2021-11-30 13:29:44 -0500

    Add files via upload

---
+++ ex0.py
@@ -0,0 +1,13 @@
+from extract import connect
+from extract import get_reviews
+
+conn = connect('data.db')
+try:
+    cursor = conn.cursor()
+    get_reviews(cursor, verb=True)
+    conn.close()
+except Exception as e:
+    print('Something went wrong...\n\n{}'.format(e))
+    conn.close()
+
+print('hello world')
